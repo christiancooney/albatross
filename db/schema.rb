586e33ae2c607cfc03e1f9fcffe0faf04b163728
christian cooney <christiancooney@hotmail.com>
2023-07-29 15:51:43 +0200

    removed acts as taggable migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_22_120733) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_112239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,12 +111,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_22_120733) do
     t.text "method8"
     t.text "method9"
     t.text "method10"
-    t.float "price"
-    t.float "atmosphere"
-    t.float "drinks"
-    t.float "food"
-    t.float "service"
-    t.float "value"
     t.text "recipe_tags"
     t.text "travel_tags"
     t.boolean "vegan"
@@ -141,6 +135,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_22_120733) do
     t.text "website"
     t.text "address"
     t.text "health_tags"
+    t.float "price"
+    t.float "atmosphere"
+    t.float "drinks"
+    t.float "food"
+    t.float "service"
+    t.float "value"
     t.integer "servings"
     t.integer "prep_time"
     t.integer "cooking_time"
@@ -172,37 +172,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_22_120733) do
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
-  create_table "taggings", force: :cascade do |t|
-    t.bigint "tag_id"
-    t.string "taggable_type"
-    t.bigint "taggable_id"
-    t.string "tagger_type"
-    t.bigint "tagger_id"
-    t.string "context", limit: 128
-    t.datetime "created_at", precision: nil
-    t.string "tenant", limit: 128
-    t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
-    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
-    t.index ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable_type_and_taggable_id"
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
-    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
-    t.index ["tagger_type", "tagger_id"], name: "index_taggings_on_tagger_type_and_tagger_id"
-    t.index ["tenant"], name: "index_taggings_on_tenant"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -220,5 +189,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_22_120733) do
   add_foreign_key "articles", "countries"
   add_foreign_key "articles", "locations"
   add_foreign_key "locations", "countries"
-  add_foreign_key "taggings", "tags"
 end
